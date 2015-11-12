6ed6e8014d0c2a6fd13c0a5fdc958ebdbd504c8b
Jessime <jessime.kirk@gmail.com>
2015-11-12 00:12:23 -0500

    Level 1 story written and download button works!

--- ExcisionApp.py
+++ ExcisionApp.py
@@ -5,7 +5,7 @@ Created on Fri Oct 23 18:52:28 2015
 @author: jessime
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, request
 
 app = Flask(__name__)
 
@@ -27,6 +27,22 @@ def about():
 @app.route('/elements')
 def elements():
     return render_template('elements.html')
+
+@app.route('/launch', methods=['GET','POST'])
+def launch():
+    if request.method == 'POST':
+        if request.form['p-value'] == '0.1':
+            print 'whoot!'
+        else:
+            print 'poo...'
+    return render_template('launch.html')
+
+@app.route('/pollution_npy')
+def pollution_npy():
+    return send_file('data/pollution_npy.zip',
+                     mimetype='application/zip',
+                     attachment_filename='pollution_npy.zip',
+                     as_attachment=True)
     
 if __name__ == "__main__":
     app.run(debug=True)
