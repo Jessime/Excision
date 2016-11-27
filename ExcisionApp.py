22a2db3e8cef287754040cfd132aa45c20e4f449
Jessime <jessime.kirk@gmail.com>
2016-11-27 13:23:12 -0500

    Saving state from last year

--- ExcisionApp.py
+++ ExcisionApp.py
@@ -4,42 +4,57 @@ Created on Fri Oct 23 18:52:28 2015
 
 @author: jessime
 """
+import flask as f
+from authomatic.adapters import WerkzeugAdapter
+from authomatic import Authomatic
+from config import CONFIG
 
-from flask import Flask, render_template, send_file, request, Response
-
-app = Flask(__name__)
-
-GOOGLE_ID = '683188869719-rh2ggaee3jovi6vni5k0rebj51o4u1mv.apps.googleusercontent.com'
-GOOGLE_SECRET = 'VxRSb62WkhQdse6z7BPe3mR-'
+app = f.Flask(__name__)
+# Instantiate Authomatic.
+authomatic = Authomatic(CONFIG, 'your secret string', report_errors=True)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return f.render_template('index.html')
 
 @app.route('/story')
 def story():
-    return render_template('story.html')
+    return f.render_template('story.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return f.render_template('about.html')
 
 @app.route('/elements')
 def elements():
-    return render_template('elements.html')
-
+    return f.render_template('elements.html')
+    
+@app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+def login(provider_name):
+    """
+    Login as described at:
+    http://peterhudec.github.io/authomatic/examples/flask-simple.html
+    """
+    response = f.make_response()
+    result = authomatic.login(WerkzeugAdapter(f.request, response), provider_name)
+    if result:
+        if result.user:
+            result.user.update()
+        return f.render_template('login.html', result=result)    
+    return response  
+    
 @app.route('/launch', methods=['GET','POST'])
 def launch():
-    if request.method == 'POST':
-        if request.form['p-value'] == '0.1':
-            return Response('whoot!')
+    if f.request.method == 'POST':
+        if f.request.form['p-value'] == '0.1':
+            return f.Response('whoot!')
         else:
-            return Response('whoot!')
-    return render_template('launch.html')
+            return f.Response('whoot!')
+    return f.render_template('launch.html')
 
 @app.route('/pollution_npy')
 def pollution_npy():
-    return send_file('data/pollution_npy.zip',
+    return f.send_file('data/pollution_npy.zip',
                      mimetype='application/zip',
                      attachment_filename='pollution_npy.zip',
                      as_attachment=True)
