1442c81a90266f2a4b22794e23d758c35700d4de
Jessime <jessime.kirk@gmail.com>
2016-12-05 00:41:35 -0500

    The submit button now runs the submission.
    Extended the tutorial.
    Added resources to the home page

--- ExcisionApp.py
+++ ExcisionApp.py
@@ -5,11 +5,13 @@ Created on Fri Oct 23 18:52:28 2015
 @author: jessime
 """
 
-from flask import Flask, render_template, send_file, request, redirect
-from subprocess import check_call, CalledProcessError
+import subprocess as sp
+
+from flask import Flask, render_template, send_file, request
 
 from pick_file import pick
 
+from os.path import isfile
 
 class Data():
     """Resposible for keeping track of the state of the app."""
@@ -44,27 +46,42 @@ def story():
 
 @app.route('/tutorial', methods=['GET', 'POST'])
 def tutorial():
+    syntax_error = None
+    semantics_error = None
     if request.method == 'POST':
         Data.submission_form = next(request.form.keys())
         Data.script = pick()
         cmd = 'python {} ATATATATGGGGGC'.format(Data.script)
-        result = None
         try:
-            result = check_call(cmd.split())
-        except CalledProcessError:
-            print(type(result))
-        return redirect('/tutorial')
-    return render_template('tutorial.html')
+            sp.run(cmd.split(), stderr=sp.PIPE, check=True)
+        except sp.CalledProcessError as e:
+            syntax_error = e.stderr.decode("utf-8")
+        if syntax_error is None:
+            if not isfile('results/tutorial/gc.txt'):
+                semantics_error = 'Your program did not produce a file in the proper location'
+            else:
+                with open('results/tutorial/gc.txt') as infile:
+                    result = infile.read()
+                    if not result:
+                        semantics_error = 'There is nothing in the file you created'
+                    elif result != '42%\n':
+                        semantics_error = 'Your answer is not correct.'
+                    else:
+                        semantics_error = 'Good job! There was no error!'
+        #return redirect(url_for('.tutorial', error=error))
+    return render_template('tutorial.html',
+                           syntax_error=syntax_error,
+                           semantics_error=semantics_error)
 
 
 
 
-@app.route('/pollution_npy')
-def pollution_npy():
-    return send_file('data/pollution_npy.zip',
-                     mimetype='application/zip',
-                     attachment_filename='pollution_npy.zip',
-                     as_attachment=True)
+#@app.route('/pollution_npy')
+#def pollution_npy():
+#    return send_file('data/pollution_npy.zip',
+#                     mimetype='application/zip',
+#                     attachment_filename='pollution_npy.zip',
+#                     as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
