61d50e7d6232d3b26abc1cfef7d84140af949f59
Jessime <jessime.kirk@gmail.com>
2016-12-28 18:36:57 -0500

    Moving processing to state.py and making minor text changes.

--- ExcisionApp.py
+++ ExcisionApp.py
@@ -5,19 +5,12 @@ Created on Fri Oct 23 18:52:28 2015
 @author: jessime
 """
 
-import subprocess as sp
+import sys
 
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, request
 
 from pick_file import pick
-
-from os.path import isfile
-
-class Data():
-    """Resposible for keeping track of the state of the app."""
-
-    submission_form = None
-    script = None
+from state import Tutorial, Data
 
 app = Flask(__name__)
 
@@ -46,42 +39,19 @@ def story():
 
 @app.route('/tutorial', methods=['GET', 'POST'])
 def tutorial():
-    syntax_error = None
-    semantics_error = None
     if request.method == 'POST':
-        Data.submission_form = next(request.form.keys())
-        Data.script = pick()
-        cmd = 'python {} ATATATATGGGGGC'.format(Data.script)
-        try:
-            sp.run(cmd.split(), stderr=sp.PIPE, check=True)
-        except sp.CalledProcessError as e:
-            syntax_error = e.stderr.decode("utf-8")
-        if syntax_error is None:
-            if not isfile('results/tutorial/gc.txt'):
-                semantics_error = 'Your program did not produce a file in the proper location'
-            else:
-                with open('results/tutorial/gc.txt') as infile:
-                    result = infile.read()
-                    if not result:
-                        semantics_error = 'There is nothing in the file you created'
-                    elif result != '42%\n':
-                        semantics_error = 'Your answer is not correct.'
-                    else:
-                        semantics_error = 'Good job! There was no error!'
+        Tutorial.script = pick()
+        Tutorial.process_post(next(request.form.keys()))
         #return redirect(url_for('.tutorial', error=error))
     return render_template('tutorial.html',
-                           syntax_error=syntax_error,
-                           semantics_error=semantics_error)
-
-
-
-
-#@app.route('/pollution_npy')
-#def pollution_npy():
-#    return send_file('data/pollution_npy.zip',
-#                     mimetype='application/zip',
-#                     attachment_filename='pollution_npy.zip',
-#                     as_attachment=True)
+                           syntax_error1=Tutorial.syntax_error1,
+                           semantics_error1=Tutorial.semantics_error1,
+                           syntax_error2=Tutorial.syntax_error2,
+                           semantics_error2=Tutorial.semantics_error2,
+                           hint_solved1=Tutorial.hint_solved1,
+                           hint_solved2=Tutorial.hint_solved2)
 
 if __name__ == "__main__":
+    if sys.version_info[0] != 3:
+        assert False, 'Python version must be 3.x'
     app.run(debug=True)
